e0eb2d1c87817162e2d3b21c1027076e4a5f4b5d
norvalbv <benjinorval@gmail.com>
2022-04-06 19:43:31 +0100

    fixed routes

--- client/src/components/navbar/sortquestions.jsx
+++ client/src/components/navbar/sortquestions.jsx
@@ -1,15 +1,24 @@
 import '../../styles/navbar/navbar.scss';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { updateQType } from '../../features/questionTypeSlice';
 
 export const SortQuestions = () => {
   const dispatch = useDispatch();
+  const questionType = useSelector((state) => state.questionType);
 
   return (
     <>
       <ul className="sort-questions">
-        <li onClick={() => dispatch(updateQType('questions'))}>Questions</li>
-        <li onClick={() => dispatch(updateQType('syntax'))}> Syntax</li>
+        <li
+          onClick={() => dispatch(updateQType('questions'))}
+          id={questionType === 'questions' ? 'questions' : null}>
+          Questions
+        </li>
+        <li
+          onClick={() => dispatch(updateQType('syntax'))}
+          id={questionType === 'syntax' ? 'syntax' : null}>
+          Syntax
+        </li>
       </ul>
     </>
   );
