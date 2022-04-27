a5bb74648c3cf07d08dee9a58c3ba712d225c39c
norvalbv <benjinorval@gmail.com>
2022-04-27 11:52:48 +0100

    minor changes

--- config/passport.js
+++ config/passport.js
@@ -5,16 +5,27 @@ const pool = require("./db/pool");
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(
+const customFields = {
+  usernameField: "uname",
+  passwordField: "pw",
+};
+
+passport.use(
+  "local",
   new LocalStrategy(function (username, password, done) {
-    db.users.findByUsername(username, (err, user) => {
-      if (err) return done(err);
+    pool.query(
+      `SELECT id, username, password from users where username=$1`,
+      [username],
+      (username,
+      (err, user) => {
+        if (err) return done(err);
 
-      if (!user) return done(null, false);
+        if (!user) return done(null, false);
 
-      if (user.password != password) return done(null, false);
+        if (user.password != password) return done(null, false);
 
-      return done(null, user);
-    });
+        return done(null, user);
+      })
+    );
   })
 );
